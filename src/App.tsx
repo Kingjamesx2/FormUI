fa09dfe7a8d744e8a401ee67c3ea083a89de08ee
Kingjamesx2 <jaafaber@gmail.com>
2024-07-05 12:11:19 -0600

    styling for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 // App.tsx
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard/Dashboard';
 import AnnualAcademicReport from './pages/Reports/AnnualAcademicReport';
 import AnnualNonAcademicReport from './pages/Reports/AnnualNonAcademicReport';
 import KeyStatisticsTemplate from './pages/Reports/KeyStatisticsReport';
 
+
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<div>Home Page</div>} />
-        <Route path="/annual-academic-report" element={<AnnualAcademicReport />} />
-        <Route path="/annual-non-academic-report" element={<AnnualNonAcademicReport />} />
-        <Route path="/key-statistics-report" element={<KeyStatisticsTemplate />} />
-      </Routes>
-    </Router>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<div>Home Page</div>} />
+    //     <Route path="/annual-academic-report" element={<AnnualAcademicReport />} />
+    //     <Route path="/annual-non-academic-report" element={<AnnualNonAcademicReport />} />
+    //     <Route path="/key-statistics-report" element={<KeyStatisticsTemplate />} />
+    //   </Routes>
+    // </Router>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={< Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+    
   );
 };
 
--- src/components/UserPosition/UserPosition.tsx
+++ src/components/UserPosition/UserPosition.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './UserPosition.scss';
 
 // Define the interface for the component props
 interface UserPositionProps {
